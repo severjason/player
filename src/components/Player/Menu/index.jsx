cabb8d26a2493a104ba0351f023bcdcdd4bb1cf4
severjason <ssever2010@gmail.com>
2018-05-15 17:14:42 +0300

    Player menu styling

--- src/components/Player/Menu/index.jsx
+++ src/components/Player/Menu/index.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
+import { MdQueueMusic, MdSearch } from 'react-icons/lib/md'
+// import { mainTheme } from '../../../styles/themes';
+import { Link } from 'react-router-dom';
 
-const Wrapper = styled('div')`
+const MenuStyle = styled('div')`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 2rem;
+  line-height: 2rem;
   
+  a {
+    font-size: 1.5rem;
+    display: block;
+  }
 `;
 
 const PlayerMenu = () => {
   return (
-      <Wrapper>
-        Player Menu
-      </Wrapper>
+    <MenuStyle>
+      <Link to={`/player`}><MdQueueMusic/></Link>
+      <Link to={`/search`}><MdSearch/></Link>
+    </MenuStyle>
   )
 };
 
