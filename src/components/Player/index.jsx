b33b726febef1853fee1b28391891a4c1539e1d7
severjason <ssever2010@gmail.com>
2018-05-15 17:14:16 +0300

    Added basic Player and Playlist components

---
+++ src/components/Player/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import PlayerMenu from './Menu';
+
+const PlayerWrapper = styled('div')`
+  
+`;
+
+const Player = () => {
+  return (
+    <PlayerWrapper>
+      <PlayerMenu/>
+    </PlayerWrapper>
+  )
+};
+
+export default Player;
---
+++ src/components/Playlist/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PlaylistWrapper = styled('div')`
+  
+`;
+
+const Playlist = () => {
+  return (
+    <PlaylistWrapper>
+      Playlist
+    </PlaylistWrapper>
+  )
+};
+
+export default Playlist;
