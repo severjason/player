4ca8bbeac2c6070601d6411c5772785dd9d7786c
severjason <ssever2010@gmail.com>
2018-05-15 17:10:54 +0300

    Added routing and global styling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import styled from 'styled-components';
 import './index.css';
+import { mainTheme } from './styles/themes';
 import { Home } from './containers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+const GlobalStyle = styled.div`
+  a {
+    color: ${props => props.theme.mainTextColor};
+    text-decoration: none;
+    
+    &:hover, &.active {
+      color: ${props => props.theme.secondaryColor};
+    }
+  }
+`;
+
+ReactDOM.render(
+  <BrowserRouter>
+    <GlobalStyle theme={mainTheme}>
+      <Home/>
+    </GlobalStyle>
+  </BrowserRouter>,
+  document.getElementById('root'));
 registerServiceWorker();
