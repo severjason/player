c3f2a9a84892ef051c2213106e86db0ba59a36f4
severjason <ssever2010@gmail.com>
2018-05-15 17:13:36 +0300

    Removed unused imports and added Player import

--- src/components/index.js
+++ src/components/index.js
@@ -1,20 +1,5 @@
-import PlayerContent from './Player/Content';
-import PlayerControls from './Player/Controls';
-import PlayerMenu from './Player/Menu';
-import PlayerSlider from './Player/Slider';
-import PlaylistHeader from './Playlist/Header';
-import PlaylistSong from './Playlist/Song';
-import SearchResults from './Search/Results';
-import SearchedSong from './Search/Song';
+import Player from './Player';
 
 export {
-  PlayerContent,
-  PlayerControls,
-  PlayerMenu,
-  PlayerSlider,
-  PlaylistHeader,
-  PlaylistSong,
-  SearchResults,
-  SearchedSong,
+  Player,
 }
-
