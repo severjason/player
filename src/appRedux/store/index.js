f2ce0a0960d3bb7288be2bd1835333960650b559
severjason <ssever2010@gmail.com>
2018-05-30 17:23:55 +0300

    added persistor

--- src/appRedux/store/index.js
+++ src/appRedux/store/index.js
@@ -2,15 +2,25 @@ import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga'
 import rootReducer from 'appRedux/reducers/index';
 import watchAll from 'appRedux/saga';
+import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(
-  rootReducer,
+const persistConfig = {
+  key: 'awesome-player',
+  storage: storage,
+  stateReconciler: autoMergeLevel2
+};
+const persistAppReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+  persistAppReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(sagaMiddleware)
 );
 
 sagaMiddleware.run(watchAll);
 
-export default store;
+export const persistor = persistStore(store);
--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import  { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
-import store from 'appRedux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from 'appRedux/store';
 import { mainTheme, GlobalStyle } from 'styles';
 import Routes from 'routes';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <ThemeProvider theme={mainTheme}>
-        <GlobalStyle>
-          <Routes/>
-        </GlobalStyle>
-      </ThemeProvider>
-    </BrowserRouter>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter>
+        <ThemeProvider theme={mainTheme}>
+          <GlobalStyle>
+            <Routes/>
+          </GlobalStyle>
+        </ThemeProvider>
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById('root'));
 registerServiceWorker();
