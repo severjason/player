7baefbcea0f65a27b933edfe9e92d330eee14cd1
severjason <ssever2010@gmail.com>
2018-06-08 11:04:59 +0300

    added customMiddleware

--- src/appRedux/store/index.js
+++ src/appRedux/store/index.js
@@ -2,11 +2,23 @@ import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga'
 import rootReducer from 'appRedux/reducers/index';
 import watchAll from 'appRedux/saga';
+import { TOGGLE_PLAYING } from 'appRedux/actions/types';
 //import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
 import storage from 'redux-persist/lib/storage';
 import { persistStore, persistReducer } from 'redux-persist';
 
 const sagaMiddleware = createSagaMiddleware();
+
+const customMiddleware = store => next => action => {
+  if (action.type === TOGGLE_PLAYING) {
+    let updatedAction = {
+      ...action,
+      created: new Date(),
+    };
+    next(updatedAction);
+  }
+  next(action);
+};
 
 const persistConfig = {
   key: 'awesome-player',
@@ -18,7 +30,7 @@ const persistAppReducer = persistReducer(persistConfig, rootReducer);
 export const store = createStore(
   persistAppReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(sagaMiddleware)
+  applyMiddleware(sagaMiddleware, customMiddleware),
 );
 
 sagaMiddleware.run(watchAll);
