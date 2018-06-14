cff7ccb4ad80a0e516cc6420cac87006469fe7ca
severjason <ssever2010@gmail.com>
2018-06-14 23:04:25 +0300

    added firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const config = {
+	apiKey: "AIzaSyCTjdIPv5vdw05Wa5jOzNnkeyDt2XpeeNA",
+	authDomain: "awesome-player.firebaseapp.com",
+	databaseURL: "https://awesome-player.firebaseio.com",
+	projectId: "awesome-player",
+	storageBucket: "awesome-player.appspot.com",
+	messagingSenderId: "220944432398",
+};
+
+export default config;
