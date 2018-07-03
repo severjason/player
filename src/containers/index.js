c7a069212f691d487d72af907b0af71e78afb0fa
severjason <ssever2010@gmail.com>
2018-07-03 09:58:43 +0300

    added asyncComponent

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,9 +1,11 @@
 import Search from './Search';
 import Player from './Player';
 import Login from './Login';
+import asyncComponent from './AsyncComponent';
 
 export {
   Search,
   Player,
   Login,
+  asyncComponent,
 };
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import { SearchPage, HomePage, LoginPage } from 'pages';
+import { LoginPage } from 'pages';
 import PrivateRoute from 'routes/private';
+import { asyncComponent } from 'containers';
 
 const Routes = () => {
   return (
     <div className='container'>
       <Switch>
-        <PrivateRoute path="/player" component={HomePage}/>
-        <PrivateRoute path="/search" component={SearchPage}/>
+        <PrivateRoute path="/player" component={asyncComponent(()=>import('pages/HomePage'))}/>
+        <PrivateRoute path="/search" component={asyncComponent(()=>import('pages/SearchPage'))}/>
         <Route path="/login" render={(route) => {
           return <LoginPage hash={route.location.hash}/>;
         }} />
